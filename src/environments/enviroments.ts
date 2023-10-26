5bee9e131c05090a19dd86992a9f2061e02af949
dvJoshr <joseph.64@outlook.com>
2023-10-25 20:11:00 -0500

    Cambios

--- src/environments/enviroments.ts
+++ src/environments/enviroments.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  HOST_API: "localhost:3000/api",
+  HOST_API: "master--idyllic-speculoos-4c3f40.netlify.app/api",
   PORT_API: null,
-  PROTOCOL_API: "http",
+  PROTOCOL_API: "https",
   production: false,
 };
