a366ed16c135df354e386ce18d29522b5109b0d7
dvJoshr <joseph.64@outlook.com>
2023-12-05 00:49:14 -0500

    Cambio variables de entorno

--- src/environments/enviroments.ts
+++ src/environments/enviroments.ts
@@ -5,9 +5,9 @@ export const environment = {
   production: false,
 };
 
-export const environment = {
-  HOST_API: "localhost:3000/api",
-  PORT_API: null,
-  PROTOCOL_API: "http",
-  production: false,
-};
+// export const environment = {
+//   HOST_API: "localhost:3000/api",
+//   PORT_API: null,
+//   PROTOCOL_API: "http",
+//   production: false,
+// };
