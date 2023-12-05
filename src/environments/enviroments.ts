b814e290d6d2536461d27cd547c81ff56c898ce2
dvJoshr <joseph.64@outlook.com>
2023-12-05 00:40:21 -0500

    Cambio variables de entorno

--- src/environments/enviroments.ts
+++ src/environments/enviroments.ts
@@ -1,9 +1,9 @@
-// export const environment = {
-//   HOST_API: "api-service-sistema.onrender.com/api",
-//   PORT_API: null,
-//   PROTOCOL_API: "https",
-//   production: false,
-// };
+export const environment = {
+  HOST_API: "api-service-sistema.onrender.com/api",
+  PORT_API: null,
+  PROTOCOL_API: "https",
+  production: false,
+};
 
 export const environment = {
   HOST_API: "localhost:3000/api",
